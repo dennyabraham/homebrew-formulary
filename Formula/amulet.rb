45addd0864a540cea38d8e1b424c3442ff4d9863
dennyabraham <dennyabraham@users.noreply.github.com>
2023-11-14 21:16:08 -0500

    feat: amulet tap

---
+++ Formula/amulet.rb
@@ -0,0 +1,20 @@
+class Amulet < Formula
+  desc "Amulet is an ML-like language that compiles to Lua"
+  homepage "amulet.works"
+  url "https://github.com/dennyabraham/amulet/archive/refs/tags/1.0.1.0a.tar.gz"
+  sha256 "32c38b7aa6b341b8300216ecb96b3f101bb839a21297613ef18d743e3b9342f7"
+  head "https://github.com/dennyabraham/amulet.git"
+
+  depends_on "haskell-stack"
+
+  def install
+
+  end
+
+  test do
+  # system "#{bin}/development", "--version"
+  # system "#{bin}/pr_app", "--version"
+  # system "#{bin}/production", "--version"
+  # system "#{bin}/staging", "--version"
+  end
+end
