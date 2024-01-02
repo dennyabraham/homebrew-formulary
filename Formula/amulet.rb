39ebbc45915c0ec1bcd8f97eadda9ac106b88304
dennyabraham <dennyabraham@users.noreply.github.com>
2024-01-02 18:52:05 -0500

    fix: lint

--- Formula/amulet.rb
+++ Formula/amulet.rb
@@ -1,20 +1,20 @@
 class Amulet < Formula
-  desc "Amulet is an ML-like language that compiles to Lua"
-  homepage "amulet.works"
-  url "https://github.com/dennyabraham/amulet/archive/refs/tags/1.0.1.0a.tar.gz"
-  sha256 "32c38b7aa6b341b8300216ecb96b3f101bb839a21297613ef18d743e3b9342f7"
-  head "https://github.com/dennyabraham/amulet.git"
+  desc 'An ML-like language that compiles to Lua'
+  homepage 'https://amulet.works'
+  url 'https://github.com/dennyabraham/amulet/archive/refs/tags/1.0.1.0a.tar.gz'
+  sha256 '32c38b7aa6b341b8300216ecb96b3f101bb839a21297613ef18d743e3b9342f7'
+  head 'https://github.com/dennyabraham/amulet.git'
 
-  depends_on "haskell-stack"
+  depends_on 'haskell-stack'
 
   def install
-
   end
 
   test do
-  # system "#{bin}/development", "--version"
-  # system "#{bin}/pr_app", "--version"
-  # system "#{bin}/production", "--version"
-  # system "#{bin}/staging", "--version"
+    system "echo 'ok'" # DELETEME
+    # system "#{bin}/development", "--version"
+    # system "#{bin}/pr_app", "--version"
+    # system "#{bin}/production", "--version"
+    # system "#{bin}/staging", "--version"
   end
 end
